63ea35fcfbd45f13f51f8e33bf664421bdd89b8c
2005Kamt <anymt2005@gmail.com>
2025-11-08 22:33:35 -0500

    Segundo commit
    agregacion de atributos, getters y setters a las clases.

---
+++ Calificacion.java
@@ -0,0 +1,16 @@
+public class Calificacion {
+    private double nota;
+
+    public Calificacion(double nota) {
+        this.nota = nota;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
+}
---
+++ Pregunta.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+public class Pregunta {
+    private int idPregunta;
+    private String enunciado;
+    private String respuestaCorrecta;
+    private List<String> opciones;
+    
+    public Pregunta(int idPregunta, String enunciado, String respuestaCorrecta, List<String> opciones) {
+        this.idPregunta = idPregunta;
+        this.enunciado = enunciado;
+        this.respuestaCorrecta = respuestaCorrecta;
+        this.opciones = opciones;
+    }
+    
+    public int getIdPregunta() {
+        return idPregunta;
+    }
+    public void setIdPregunta(int idPregunta) {
+        this.idPregunta = idPregunta;
+    }
+    public String getEnunciado() {
+        return enunciado;
+    }
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+    public String getRespuestaCorrecta() {
+        return respuestaCorrecta;
+    }
+    public void setRespuestaCorrecta(String respuestaCorrecta) {
+        this.respuestaCorrecta = respuestaCorrecta;
+    }
+    public List<String> getOpciones() {
+        return opciones;
+    }
+    public void setOpciones(List<String> opciones) {
+        this.opciones = opciones;
+    }
+}
